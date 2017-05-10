bc9750fae085bb4541d27e24a1beff58cf661d12
Fyyaz Khan <fyyazk@gmail.com>
2017-05-10 22:05:49 +0000

    works for multiple disks

--- nasinfo.php
+++ nasinfo.php
@@ -1,29 +1,33 @@
 <?php
-$df = disk_free_space("/media/NASHDD1/");
-$str = "";
+$disks = array("/", "/media/NASHDD1/");
 
+foreach ($disks as $disk) {
 
-if ($df < 1000)
-{
-	$df = $df;
-	$str = "B";
-}
-elseif ($df <1000000)
-{
-	$df = $df/1000;
-	$str = "KB";
-}
-elseif ($df < 1000000000)
-{
-	$df = $df/1000000;
-	$str = "MB";
-}
-else
-{
-	$df = $df/1000000000;
-	$str = "GB";
-}
+	$str = "";
+	$df = disk_free_space($disk);
 
-$df = round($df, 2);
-echo "$df $str left";
+	if ($df < 1000)
+	{
+		$df = $df;
+		$str = "B";
+	}
+	elseif ($df <1000000)
+	{
+		$df = $df/1000;
+		$str = "KB";
+	}
+	elseif ($df < 1000000000)
+	{
+		$df = $df/1000000;
+		$str = "MB";
+	}
+	else
+	{
+		$df = $df/1000000000;
+		$str = "GB";
+	}
+
+	$df = round($df, 2);
+	echo "$disk: $df $str left\n";
+}
 ?>
